d961ba81b65298f6156b1fa26bd2c6f9138f20ec
Dan <daniel.wright33@icloud.com>
2026-02-08 19:02:34 -0500

    Add game detail page from friend's list, batch ranking flow, and styled section headers

---
+++ PlayedIt/BatchRankFlowView.swift
@@ -0,0 +1,8 @@
+//
+//  BatchRankFlowView.swift
+//  PlayedIt
+//
+//  Created by Daniel Hankins-Wright on 2/8/26.
+//
+
+import Foundation
---
+++ PlayedIt/BatchRankSelectionView.swift
@@ -0,0 +1,8 @@
+//
+//  BatchRankSelectionView.swift
+//  PlayedIt
+//
+//  Created by Daniel Hankins-Wright on 2/8/26.
+//
+
+import Foundation
