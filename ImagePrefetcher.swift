34c6cfc8cc6acb7997285b2ea3e73572d9621799
Dan <daniel.wright33@icloud.com>
2026-03-04 12:34:44 -0500

    prefetch feed

---
+++ ImagePrefetcher.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePrefetcher.swift
+//  PlayedIt
+//
+//  Created by Daniel Hankins-Wright on 3/4/26.
+//
+
+import Foundation
---
+++ PlayedIt/FeedPreloader.swift
@@ -0,0 +1,8 @@
+//
+//  FeedPreloader.swift
+//  PlayedIt
+//
+//  Created by Daniel Hankins-Wright on 3/4/26.
+//
+
+import Foundation
